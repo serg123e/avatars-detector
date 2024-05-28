ed1c5caffcc62e27bdcbf68da94d7ad8d3f8ced4
Sergey Evstegneiev <serg123e@gmail.com>
2024-05-28 14:48:08 +0200

    Rename ENVs

--- README.md
+++ README.md
@@ -1,27 +1,36 @@
 # User avatars' scoring service
 
-## Installation
+## Run from Docker Hub
 
-  1. git clone https://github.com/serg123e/avatars-detector.git
-  2. cd avatars-detector
-  3. docker build -t avatars-detector .
-  4. docker run -p 5080:80 avatars-detector
+    docker run -p 5080:80 -e API_TOKEN=your_api_token serg123e/avatars-detector:latest
+
+## Build local image and run
+
+  1. git lfs install
+  2. git clone https://github.com/serg123e/avatars-detector.git
+  3. cd avatars-detector
+  4. docker build -t avatars-detector .
+  5. docker run -p 5080:80 -e API_TOKEN=your_api_token avatars-detector
 
 You can disable loading of some models to save memory and CPU:
       
-    docker run -e ENABLE_NUDE=false -e ENABLE_NSFW=false -p 5080:80 avatars-detector
+    docker run -e NUDE_DETECTOR=false -e NSFW_DETECTOR=false -p 5080:80 avatars-detector
 
-You can disable loading of some models to save memory and CPU:
-      
-    docker run -e ENABLE_CELEB=false -e ENABLE_NUDE=false -e ENABLE_NSFW=false -p 5080:80 avatars-detector
+## Authentication
 
+    curl -X POST "http://localhost:5080/upload" -H "Authorization: your_api_token" -F "file=@./path/to/your/file.jpg"
+
+  If the environment variable API_TOKEN is not set, you can simply use:
+
+    curl -X POST "http://localhost:5080/upload" -F "file=@./path/to/your/file.jpg"
 
 ## Usage
 
 Check [examples](examples)
 
-
 ## Training
 
 Check [training](training)
   
+## Example response
+
--- app/app.py
+++ app/app.py
@@ -1,34 +1,41 @@
 import os
 import pathlib
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.security.api_key import APIKeyHeader
 from PIL import Image
 from fastapi.middleware.cors import CORSMiddleware
 import shutil
 
+API_TOKEN = os.getenv("API_TOKEN")
+api_key_header = APIKeyHeader(name="Authorization", auto_error=False)
+
+if API_TOKEN:
+  print("API_TOKEN specified, authorization enabled") 
+else:
+  print("API_TOKEN not specified, authorization DISABLED. Do not expose the service to public")
+
 file_path = pathlib.Path(__file__).parent.absolute()
 
-AVATAR_MODEL_PATH = f'{file_path}/models/resnet_model91.81.pth'
-NSFW_MODEL_PATH = f'{file_path}/models/nsfw_mobilenet2.224x224.h5'
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+AVATAR_MODEL_PATH = os.getenv("AVATAR_MODEL_PATH", f'{file_path}/models/resnet_model91.81.pth')
+NSFW_MODEL_PATH = os.getenv("NSFW_MODEL_PATH", f'{file_path}/models/nsfw_mobilenet2.224x224.h5')
 
 # Environment variables to enable/disable models
-ENABLE_AVATAR = os.getenv('ENABLE_AVATAR', 'true').lower() == 'true'
-ENABLE_NSFW = os.getenv('ENABLE_NSFW', 'true').lower() == 'true'
-ENABLE_NUDE = os.getenv('ENABLE_NUDE', 'true').lower() == 'true'
+AVATAR_DETECTOR = os.getenv('AVATAR_DETECTOR', 'true').lower() == 'true'
+NSFW_DETECTOR = os.getenv('NSFW_DETECTOR', 'true').lower() == 'true'
+NUDE_DETECTOR = os.getenv('NUDE_DETECTOR', 'true').lower() == 'true'
 
-if ENABLE_NSFW:
+if NSFW_DETECTOR:
     import nsfw_predict
     nsfw_model = nsfw_predict.load_model(NSFW_MODEL_PATH)
     print("Loaded: nsfw_predict")
 
-if ENABLE_NUDE:
+if NUDE_DETECTOR:
     from nudenet import NudeDetector
     nude_detector = NudeDetector()
     print("Loaded: nude_detector")
 
-if ENABLE_AVATAR:
+if AVATAR_DETECTOR:
     import avatar_detector
     # from avatar_detector import classify as classify_avatar
     resnet_model = avatar_detector.load_model(AVATAR_MODEL_PATH)
@@ -45,8 +52,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def get_api_key(api_key_header: str = Depends(api_key_header)):
+    if API_TOKEN and api_key_header != API_TOKEN:
+
+        raise HTTPException(status_code=403, detail=f"Could not validate credentials")
+    return api_key_header
+
 @app.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(file: UploadFile = File(...), api_key: str = Depends(get_api_key)):
     if not file.filename:
         raise HTTPException(status_code=400, detail="No selected file")
 
@@ -58,18 +71,18 @@ async def upload_file(file: UploadFile = File(...)):
     response = {}
 
     # Process the image for classification
-    if ENABLE_AVATAR:
+    if AVATAR_DETECTOR:
         image = Image.open(temp_path).convert('RGB')
         classification_result = avatar_detector.classify(resnet_model, image)
         response['avatar_detector'] = classification_result
 
     # Predict single image
-    if ENABLE_NSFW:
+    if NSFW_DETECTOR:
         nsfw_results = nsfw_predict.classify_one(nsfw_model, temp_path)
         response['nsfw_model'] = nsfw_results
 
     # Use NudeDetector on the saved file
-    if ENABLE_NUDE:
+    if NUDE_DETECTOR:
         nudity_results = nude_detector.detect(temp_path)
         response['nudenet'] = nudity_results
 
@@ -77,6 +90,18 @@ async def upload_file(file: UploadFile = File(...)):
     os.remove(temp_path)
 
     return JSONResponse(content=response)
+
+@app.get("/", response_class=HTMLResponse)
+async def get_form():
+    html_content = """
+        <h1>Upload File to Avatar Detector</h1>
+        <form action="/upload" enctype="multipart/form-data" method="post">
+            <input name="file" type="file">
+            <input type="submit">
+        </form>
+    """
+    return HTMLResponse(content=html_content)
+
 
 if __name__ == "__main__":
     import uvicorn
